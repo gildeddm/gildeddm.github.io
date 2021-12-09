bb37c8c5d4dbdbd82c85856d394bcbba5fc8d18f
gildeddm <95612238+gildeddm@users.noreply.github.com>
2021-12-09 13:36:55 +1030

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,5 @@
   
-            /* Code for changing active 
-            link on clicking */
+/* active link on clicking */
             var btns = 
                 $("#navigation .navbar-nav .nav-link");
   
@@ -16,3 +15,31 @@
                     this.className += " active";
                 });
             }
+
+
+  /* Bootstrap tag filter  */
+$(document).ready(function() {
+
+
+  $(".btn-tag").click(function(eventObject) {
+    var tag = $(this).html();
+    var totalPanel = document.getElementsByClassName("panel").length;
+    eventObject.preventDefault();
+    for (var x = 1; x <= totalPanel; x++  ){
+      console.log("json: " + JSON.stringify($("#project" + x ).html()));
+      if(JSON.stringify($("#project" + x ).html()).indexOf(tag) >= 0 ){
+        //console.log( JSON.stringify($("#project" + x ).html()) );
+        $("#project" + x ).hide();
+        $("#project" + x ).fadeIn();
+      } else {
+        $("#project" + x ).hide();
+      };
+      //console.log(tag);
+      //console.log(JSON.stringify($("#project" + x ).html()).indexOf(tag));
+    }
+    //console.log(tag);
+
+  });
+  //console.log("total panel: " + document.getElementsByClassName("panel").length );
+
+});
